f265fb66d8e21bb9f1f49018f4b7952b8f35179e
thomas-jacque-schultz <schultz.thomas@free.fr>
2019-12-05 22:18:06 +0100

    debut santeBienEtre

---
+++ views/santeBienEtre.php
@@ -0,0 +1,53 @@
+<?php
+include_once 'header.php';
+?>
+
+    <main>
+      <div class="first">
+        <div class="doctolib">
+          <p>Médecine, rendez-vous médicaux</p>
+          <div class="docto_link">
+		<a href="https://www.doctolib.fr/">Doctolib</a>
+          </div>
+        </div>
+
+        <div class="avortement">
+          <p>Avortement, dépistage & bilan santé</p>
+          <div class="avortement_link">
+		<a href="https://www.planning-familial.org/fr">planning-familial</a>	
+          </div>
+        </div>
+
+        <div class="mutuelle_primo">
+          <p>Mutuelle LMDE</p>
+          <div class="mutuelle_primo_link">
+		<a href="https://www.lmde.fr/">LMDE</a>	
+          </div>
+        </div>
+
+        
+        <div class="mutuelle_secondo">
+          <p>Mutuelle SMERRA</p>
+          <div class="mutuelle_secondo_link">
+		<a href="https://smerra.fr/">SMERRA</a>	
+          </div>
+        </div>
+
+	<div class="medecin_mulhouse">
+          <p>Mutuelle SMERRA</p>
+          <div class="medecin_mulhouse_link">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d16456.223490552682!2d7.314322411109159!3d47.74093501079078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sm%C3%A9decin%20a%20mulhouse!5e0!3m2!1sfr!2sfr!4v1575579929206!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d42236.15881847928!2d7.7154777338239615!3d48.576146740664924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sm%C3%A9decin%20a%20strasbourg!5e0!3m2!1sfr!2sfr!4v1575580173281!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+          </div>
+        </div>
+
+
+      </div>
+
+      <div class="second">
+        <img id="student" src="student.jpg" alt="student">
+      </div>
+    </main>
+<?php
+include_once 'footer.php';
+?>
