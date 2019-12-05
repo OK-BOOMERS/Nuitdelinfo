f782b6c0ac29ff3c13a8d9ca8cd530511a558606
Manon Heyser <manon.heyser@ha.fr>
2019-12-05 21:47:28 +0100

    .

---
+++ views/main.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style0.css">
+    <link href="https://fonts.googleapis.com/css?family=Alata|Alatsi|Fredoka+One|Roboto&display=swap" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <!-- <em id="title">Au Hasard.</em> -->
+      <li id="title">Au Hasard.</li>
+      <nav>
+        <li><a href="">budget</a></li>
+        <li><a href="">logement</a></li>
+        <li><a href="">transports</a></li>
+        <li><a href="">vie pratique</a></li>
+        <li><a href="">santé / bien-être</a></li>
+        <li><a href="">culture</a></li>
+        <li><a href="">sports</a></li>
+        <li><a href="">vie associative</a></li>
+      </nav>
+    </header>
+
+    <main>
+      <div class="first">
+        <div class="carton budget un">
+          <p>budget</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton logement deux">
+          <p>logement</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton transports trois">
+          <p>transports</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton pratique quatre">
+          <p>vie pratique</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton sante cinq">
+          <p>santé / bien-être</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton culture six">
+          <p>culture</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton sports sept">
+          <p>sports</p>
+          <div class="">
+
+          </div>
+        </div>
+
+        <div class="carton associative huit">
+          <p>vie associative</p>
+          <div class="">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="second">
+        <img id="student" src="student.jpg" alt="student">
+      </div>
+    </main>
+
+    <footer>
+
+    </footer>
+  </body>
+</html>
