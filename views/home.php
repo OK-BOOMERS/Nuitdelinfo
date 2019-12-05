3ea71f93d84085e3c3b49b126ff99bdaa482c74e
Nicolas Herr <nicolas.herr2@etu.unistra.fr>
2019-12-05 19:23:02 +0100

    init

---
+++ views/home.php
@@ -0,0 +1,11 @@
+<h1>Aled²</h1>
+<nav>
+  <ul>
+    <li><a href="#">Lien 1</a></li>
+    <li><a href="#">Lien 2</a></li>
+    <li><a href="#">Lien 3</a></li>
+    <li><a href="#">Lien 4</a></li>
+  </ul>
+</nav>
+
+<p>Some bullshit</p>
