9420c9e5c38ccaa32dbef09e3772fa23f7b1c7ad
lesliecaron <lesliecaron@live.fr>
2019-12-06 02:45:07 +0100

    ajout de la nav dans le header

--- views/header.php
+++ views/header.php
@@ -8,4 +8,18 @@
     <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.1/mapbox-gl.js'></script>
     <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.1/mapbox-gl.css' rel='stylesheet' />
   </head>
+  <header>
+    <!-- <em id="title">Au Hasard.</em> -->
+    <li id="title">Au Hasard.</li>
+    <nav>
+      <li><a href="budget.php">budget</a></li>
+      <li><a href="logement.php">logement</a></li>
+      <li><a href="transport.php">transports</a></li>
+      <li><a href="">vie pratique</a></li>
+      <li><a href="santeBienEtre.php">santé / bien-être</a></li>
+      <li><a href="">culture</a></li>
+      <li><a href="sports.php">sports</a></li>
+      <li><a href="">vie associative</a></li>
+    </nav>
+  </header>
   <body>
