0bf653dada41847c625b64dc746d15448d5e5a47
lesliecaron <lesliecaron@live.fr>
2019-12-06 01:23:26 +0100

    budget , logement , transport

---
+++ views/budget.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'header.php';
+ ?>
+
+<main>
+  <h1>Budget</h1>
+  <p>Tu ne connais pas les aides dont tu pourrais bénéficier ? tu voudrais pouvoir gerer ton budget plus facilement ?   </p>
+  <div class="">
+    <h2>Des applis pour gerer son budget</h2>
+    <div class="">
+      <p>si tu es dépensier/ère , que tu a du mal a gerer ton argent , utilise une de ces applications pour t'aider à t'en sortir</p>
+      <a href="https://www.linxo.com/">Linxo</a>
+      <a href="https://bankin.com/">Bankin</a>
+
+    </div>
+  </div>
+
+
+  <div class="">
+    <h2> Les aides financières étudiantes</h2>
+    <div class="">
+      <p>ces organisment peuvent te permettre d'obtenir une bourse </p>
+      <a href="https://www.etudiant.gouv.fr/"> La bourse du crous </a>
+      <a href="http://www.crous-strasbourg.fr/">Le crous de Strasbourg</a>
+      <a href="http://www.crous-strasbourg.fr/service/pole-accueil-etudiant-mulhouse/"> Le clous de mulhouse </a>
+    
+
+    </div>
+
+
+
+</main>
+
+
+
+
+ <?php
+include_once 'footer.php';
+ ?>
